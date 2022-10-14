ff27b6bc3ea51e1a60e43a0bb8c8ff15e8333f1c
Baiel <baielkarybaev@gmail.com>
2022-10-14 10:43:15 +0600

    store

--- data.js
+++ data.js
@@ -4,59 +4,37 @@ const menu_items = [
         id: 1,
         title: 'Hamburger',
         price: 120,
-        img:  '././img/hamburger.jpg'
+        img:  'https://avatanplus.com/files/resources/original/5c79e463c15f81693c2425d0.png'
     },
     {
         id: 2,
         title: 'Coffee',
         price: 80,
-        img:  '././img/coffee.jpg'
+        img:  'https://www.pngplay.com/wp-content/uploads/9/Coffee-Background-PNG-Image.png'
     },
     {
         id: 3,
         title: 'Cola',
         price: 70,
-        img:  '././img/cola.svg'
+        img:  'https://pizzafiero.ru/wp-content/uploads/2018/02/Coca-Cola-PNG-Clipart.png'
     },
     {
         id: 4,
         title: 'Tea',
         price: 20,
-        img:  '././img/tea.svg'
+        img:  'https://freepngimg.com/thumb/categories/1008.png'
     },
     {
         id: 5,
         title: 'Cheeseburger',
         price: 100,
-        img:  '././img/cheeseburger.svg'
+        img:  'https://upload.wikimedia.org/wikipedia/commons/1/11/Cheeseburger.png'
     },
     {
         id: 6,
         title: 'Fries',
         price: 60,
-        img:  '././img/fries.svg'
-    },             {
-        id: 7,
-        title: 'Cheer',
-        price: 100,
-        img:  '././img/cheeseburger.svg'
-    },
-    {
-        id: 8,
-        title: 'topek',
-        price: 60,
-        img:  '././img/fries.svg'
-    },{
-        id: 9,
-        title: 'mopik',
-        price: 100,
-        img:  '././img/cheeseburger.svg'
-    },
-    {
-        id: 10,
-        title: 'cope',
-        price: 60,
-        img:  '././img/fries.svg'
-    },
+        img:  'https://www.freepnglogos.com/uploads/fries-png/premium-french-fries-photos-7.png'
+    }
 ]
 let orders_basket = []
